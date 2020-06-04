f0badc28dfb8e0e6761b050882067e51214430b1
beatrice68840 <beatricehatsch@gmail.com>
2020-06-04 16:08:51 +0200

    formulaire contact

--- wp-content/themes/ms/footer.php
+++ wp-content/themes/ms/footer.php
@@ -1,24 +1,28 @@
 </div>
-    <footer>
-        <div class='footer'>
-    <?php wp_nav_menu(['theme_location'=> 'footer',
-    'container' => false,
-     'menu_class'=> 'navbar-nav mr-auto',
-     
-     ]) ?>
-     <div class="row row-cols-1 row-cols-md-3">
-   
-   <p>CONTACT</br>
-    MÉLISSA STARCK</br>
-    lieu : 68100 MULHOUSE</br>
-    email : mrs.starck.english@gmail.com
-</p>
-   <p>© Site créé par BÉATRICE HATSCH </p> 
+<footer>
+    <div class='footer'>
+        <?php wp_nav_menu([
+            'theme_location' => 'footer',
+            'container' => false,
+            'menu_class' => 'navbar-nav mr-auto',
 
-     </div>
-    </footer>
-    <?php wp_footer() ?>
-    
+        ]) ?>
+        <div class="row row-cols-1 row-cols-md-3">
+            <div class='contact'>
+                <p>CONTACT</br>
+                    MÉLISSA STARCK</br>
+                    lieu : 68100 MULHOUSE</br>
+                    email : mrs.starck.english@gmail.com
+                </p>
+            </div>
+            <div class='nom'>
+                <p>© Site créé par BÉATRICE HATSCH </p>
+            </div>
+        </div>
+</footer>
+<?php wp_footer() ?>
+
 
 </body>
+
 </html>
