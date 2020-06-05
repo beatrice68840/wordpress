2d64cadfd6264b0b371196a24ad7542fdec3419d
beatrice68840 <beatricehatsch@gmail.com>
2020-06-05 11:00:21 +0200

    format email

--- wp-content/themes/ms/header.php
+++ wp-content/themes/ms/header.php
@@ -2,13 +2,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mélissa Starck</title>
+    <title>ENGLISH AT HOME</title>
     <?php wp_head() ?>
     <link href="<?php bloginfo('template_directory'); ?>/style.css" rel="stylesheet">
 </head>   
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-color=#000058 .mb-0 text-white ">
-    <a class="navbar-brand" href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/MS-42x52.png" alt="Logo">English at home</a>
+    <a class="navbar-brand" href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/MS-42x52.png" alt="Logo">ENGLISH AT HOME</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
