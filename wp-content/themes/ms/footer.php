27ffd1614716df5a2eab66a907285fe8138b2e8b
beatrice68840 <beatricehatsch@gmail.com>
2020-06-11 11:52:58 +0200

    reglage footer

--- wp-content/themes/ms/footer.php
+++ wp-content/themes/ms/footer.php
@@ -1,22 +1,25 @@
 </div>
 <footer>
+
     <div class='footer'>
         <?php wp_nav_menu([
             'theme_location' => 'footer',
-            'container' => false,
             'menu_class' => 'navbar-nav mr-auto',
 
         ]) ?>
-        <div class="row row-cols-1 row-cols-md-3">
-            <div class='contact'>
-                <p>CONTACT</br>
-                    MÉLISSA STARCK</br>
-                    lieu : 68100 MULHOUSE</br>
-                    email : mrs.starck.english@gmail.com
-                </p>
-            </div>
-            <div class='nom'>
-                <p>© Site créé par BÉATRICE HATSCH </p>
+
+        <div class="flex-row row-cols-3 row-cols-md-3">
+            <div class='container'>
+                <div class='contact'>
+                    <p>CONTACT</br>
+                        MÉLISSA STARCK</br>
+                        lieu : 68100 MULHOUSE</br>
+                        email : mrs.starck.english@gmail.com
+                    </p>
+                </div>
+                <div class='nom'>
+                    <p>© Site créé par BÉATRICE HATSCH </p>
+                </div>
             </div>
         </div>
 </footer>
--- wp-content/themes/ms/header.php
+++ wp-content/themes/ms/header.php
@@ -2,18 +2,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ENGLISH AT HOME</title>
+    <title><?php wp_title(); ?></title>
     <?php wp_head() ?>
     <link href="<?php bloginfo('template_directory'); ?>/style.css" rel="stylesheet">
 </head>   
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-color=#000058 .mb-0 text-white ">
+<nav class="navbar navbar-expand-lg navbar-dark bg-color=#000058 'flex-row' .mb-0 text-white ">
     <a class="navbar-brand" href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/MS-42x52.png" alt="Logo">ENGLISH AT HOME</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    
+ 
+
+
+
+     
+    
+ 
       <?php wp_nav_menu([
         'theme_location' => 'header',
         'container' => false,
