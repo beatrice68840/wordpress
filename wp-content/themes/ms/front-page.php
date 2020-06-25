d6325ddf7cbe4a994b7abbc97ad092da1346330a
beatrice68840 <beatricehatsch@gmail.com>
2020-06-25 11:40:20 +0200

    banner termine

--- wp-content/themes/ms/front-page.php
+++ wp-content/themes/ms/front-page.php
@@ -1,11 +1,7 @@
 <?php get_header() ?>
 <!--code pour inserrer la bannière-->
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <div class='banner .mb-0 col-2'>
-            
-                <img src="<?php echo get_template_directory_uri(); ?>/img/banner.jpg" alt="Logo">
-         
-        </div>
+
             <h1><?php the_title() ?></h1>
             <!--la mettre l'image -->
             <?php the_content() ?>
